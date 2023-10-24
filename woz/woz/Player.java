d01877e8947b369d436ef7e50f31d181ad58ea85
batzcoding <144021993+batzcoding@users.noreply.github.com>
2023-10-24 16:07:13 +0200

    Update Player.java
    Made Setter and Getter methods for Player

--- woz/woz/Player.java
+++ woz/woz/Player.java
@@ -1,6 +1,34 @@
 /*Klassen her skal bruges til at vi har en mand
 som kan have POINTS, INVENTORY og skulle kunne DROP og PICK UP diverse ting*/
 public class Player {
+    //integers (Points)
+    //Inventory
+    private int points;
+    private String name;
+
+
+    //Constructor for Player
+    public Player(int points, String name){
+        this.points=points;
+        this.name=name;
+    }
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public int getPoints(){
+        return points;
+    }
+
+    //Setter (Updating points)
+    public void setPoints(int NewPoint){
+        points=NewPoint;
+    }
+
+    //Methods for drop() and pickup()
+
+
+
 
 
 }
