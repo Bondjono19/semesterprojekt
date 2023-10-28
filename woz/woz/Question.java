a507f3d91f9fecfa96e20c81e177c0abcb4ba46c
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-10-28 13:25:28 +0200

    new class

---
+++ woz/woz/Question.java
@@ -0,0 +1,15 @@
+public class Question{
+
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+
+    public Question(String q, String a1, String a2, String a3){
+        this.question = q;
+        this.answer1  = a1;
+        this.answer2  = a2;
+        this.answer3  = a3;
+    }
+
+}
