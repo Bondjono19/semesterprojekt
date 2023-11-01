754b6e6c9f668bae0349243244e8471bb3b78bd4
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-11-01 17:59:16 +0100

    commandpour

--- woz/woz/CommandPour.java
+++ woz/woz/CommandPour.java
@@ -7,12 +7,21 @@ public class CommandPour extends BaseCommand implements Command{
         ArrayList<Item> inventory = context.getInventory().getInventoryContents();
         int amount = 0;
         if(area.name.equals("Water Reservoir")){
+
             for(int i = 0;i<inventory.size();i++){
                 if(inventory.get(i).getFiltered()[0]==true && inventory.get(i).getFiltered()[1] && inventory.get(i).getFiltered()[2]==true && inventory.get(i).getIsFull()==true){
                     amount = amount + inventory.get(i).getItemCapacity();
+                    inventory.get(i).setFiltered(0, false);
+                    inventory.get(i).setFiltered(1, false);
+                    inventory.get(i).setFiltered(2, false);
+                    inventory.get(i).fill(false);
+                    System.out.println("Water poured from item " + inventory.get(i).getItemName());
                 }
             }
             area.setWaterAmount(area.getWaterAmount()+amount);
+        }
+        else{
+            System.out.println("Can't pour water here!");
         }
 
     }
