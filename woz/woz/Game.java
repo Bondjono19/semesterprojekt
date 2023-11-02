4f9ed3f6cf4f4125e03567880dbd2b1a9aba3761
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-11-02 17:03:52 +0100

    Update Game.java

--- woz/woz/Game.java
+++ woz/woz/Game.java
@@ -52,7 +52,7 @@ class Game {
       System.out.print("> ");
       String line = scanner.nextLine();
 
-      if (context.getCurrent().getName().contains("niggercock")) {
+      if (context.getCurrent().getName().contains("eeeee")) {
         // Prompt the player with a question and options in the Water Reservoir
         System.out.println("You've entered the Water Reservoir.");
         System.out.println("Answer this question:");
