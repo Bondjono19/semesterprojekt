8f1aceba7cf46d6aa5ce4daa6b766475c1b43e66
Bondjono19 <144017805+Bondjono19@users.noreply.github.com>
2023-11-02 17:18:05 +0100

    sauhuwsaca

--- woz/woz/CommandDrop.java
+++ woz/woz/CommandDrop.java
@@ -6,24 +6,28 @@ public class CommandDrop extends BaseCommand implements Command{
     public void execute (Context context, String command, String parameters[]){
         Inventory inventory= context.getInventory();
             if (parameters.length<2){
-                System.out.println("drop one item:" + inventory.getInventoryContents());
+                System.out.println("drop one item, type number to pick item from list displayed: ");
+                for (int i = 0;i<inventory.getInventoryContents().size();i++){
+                    System.out.println(inventory.getInventoryContents().get(i).getItemName());
+                }
             }
             try{
                 Scanner s = new Scanner(System.in);
                 int choice= s.nextInt();
                 if (choice==1){
                     System.out.println("First item in inventory has been removed");
-                    inventory.getInventoryContents().remove(1);
+                    inventory.getInventoryContents().remove(0);
                 }
 
-                if (choice==2 && inventory.getInventoryContents().size()<2){
-                    inventory.getInventoryContents().remove(2);
+                if (choice==2){
+                    inventory.getInventoryContents().remove(1);
                     System.out.println("Second item in inventory has been removed");
                 }
 
 
-
-            }catch (Exception e){System.out.println("This item doesn't exist in your inventory or you have no items to drop");}
+            }catch (Exception e){
+                System.out.println("This item doesn't exist in your inventory or you have no items to drop");
+            }
 
 
     }
--- woz/woz/CommandStats.java
+++ woz/woz/CommandStats.java
@@ -3,7 +3,10 @@ public class CommandStats extends BaseCommand implements Command {
     public void execute(Context context,String command, String parameters[]){
 
         System.out.println("You have: "+context.getPlayer().getPoints()+" points.");
-
+        System.out.println("You have the following items in your inventory: ");
+        for (int i = 0;i<context.getInventory().getInventoryContents().size();i++){
+            System.out.println("- " + context.getInventory().getInventoryContents().get(i).getItemName());
+        }
     }
 
 }
